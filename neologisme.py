e5450cc86b4699a173e3bca2e58d61af80b71f38
Ilona <ilolab15@gmail.com>
2022-09-09 12:13:59 +0200

    Tableau de proba fonctionnonel

--- neologisme.py
+++ neologisme.py
@@ -1,8 +1,11 @@
+# tableaux avec proba cummulés
+
 def recupererTableau(chemain:str = "./frdic.txt"):
     
     # lire le fichier et le mettre dans une liste
     tableau = []
     fichier1 = open(chemain)
+
     for ligne in fichier1:
         tableau.append(ligne.replace("\n", "", 1))
 
@@ -12,31 +15,7 @@ def compterLettres(tableau:list):
 
     alphabetOffest = 97
 
-    tableauOccurrences = [[0] * 26] * 26
-
-    """# methode lente 
-    # boucle sur tout l'alphabet
-    # TODO: opptimiser la boucle n**4
-    for tlx in range(26):
-        lettrex = chr(tlx + alphabetOffest)
-
-        for tly in range(26):
-            lettrey = chr(tly + alphabetOffest)
-
-            for mot in tableau:
-
-                # len(mot) - 1 car on a pas besoin de regarder la lettre suivante si c'est la derniere
-                for i in range(len(mot) - 1):
-
-                    lettre = mot[i]
-
-                    if(lettre == lettrex):
-
-                        lettreSuivante = mot[i + 1]
-
-                        if(lettreSuivante == lettrey):
-
-                            tableauOccurrences[tlx][tly] += 1"""
+    tableauOccurrences = [[0 for x in range(26)] for x in range(26)]
 
     # methode n² * plus rapide
     for mot in tableau:
@@ -45,20 +24,27 @@ def compterLettres(tableau:list):
             lettre = mot[i]
             tlx = ord(lettre) - alphabetOffest
 
-            lettreSuivante = mot[i + 1]
+            lettreSuivante = mot[(i + 1)]
             tly = ord(lettreSuivante) - alphabetOffest
 
-            if(0 <= tlx <= 26 and 0 <= tly <= 26):
+            if(0 <= tlx <= 25 and 0 <= tly <= 25):
                 tableauOccurrences[tlx][tly] += 1
 
-            return tableauOccurrences
-
-    # uniformiser le tableau des occurrences
+    # uniformiser le tableau des occurrences (en %)
     for iox in range(len(tableauOccurrences)):
         for ioy in range(len(tableauOccurrences[iox])):
-            tableauOccurrences[iox][ioy] = int((tableauOccurrences[iox][ioy] / sum(tableauOccurrences[iox])) * 100)
+            tableauOccurrences[iox][ioy] = (tableauOccurrences[iox][ioy] / sum(tableauOccurrences[iox])) * 100
+            pass
 
     return tableauOccurrences
+
+def nouveauMot(nombreDeLettres):
+    if(nombreDeLettres <= 2):
+        raise ValueError('Mot de moins ou egal a 2 lettres dans le programme')
+
+    print("erreur passée")
+
+    return
 
 # entrée du program 
 def main():
@@ -66,10 +52,8 @@ def main():
     tab = recupererTableau()
     tabOccu = compterLettres(tab)
 
-    # afficher le tableau
-    # print(tabOccu)
-
-    for line in tabOccu:
-        print(line)
+    #for line in tabOccu:
+    #    print(line)
+    nouveauMot(5)
 
 main()
